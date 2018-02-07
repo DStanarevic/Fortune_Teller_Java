95bdd83bc745be1c286e9ceacc9f2f30f4ba6c1f
DStanarevic <dejanstanarevic@gmai.com>
2018-02-07 14:36:50 -0500

    User input added.

--- Fortune_Teller_Java/src/Fortune_Teller.java
+++ Fortune_Teller_Java/src/Fortune_Teller.java
@@ -19,16 +19,43 @@ public class Fortune_Teller {
          String transportMethod;
          String firstName;
          String lastName;
+         String color;
          
-         //Storyline
+         //User Input
          System.out.println("You are at a Carnival and come across a fortune teller, they ask you for your name.");
          firstName = consoleReadline.nextLine().trim();
          
-         System.out.println("The fortune teller than asks you for your last name.");
+         System.out.println("What is your last name?");
          lastName = consoleReadline.nextLine().trim();
          
-         System.out.println("The fortune teller asks you how old you are.");
+         System.out.println("How old are you?");
          age = Integer.parseInt(consoleReadline.nextLine().trim());
+         
+         System.out.println("In what month were you born in? (Enter as number)");
+         month = Integer.parseInt(consoleReadline.nextLine().trim());
+         
+         System.out.println("What is your for your favorite ROYGBIV color? (Enter help for more info.)");
+         color = consoleReadline.nextLine().toLowerCase();
+         
+         if(color == "help")
+         {
+        	 System.out.println("Please choose between Red, Orange, Yellow, Green, Blue, Indigo, Violet");
+        	 color = consoleReadline.nextLine().toLowerCase();
+         }
+         
+         System.out.println("How many siblings do you have?"); 
+         sibling = Integer.parseInt(consoleReadline.nextLine().trim());
+         
+         //Retirement calculation.
+         if(age %2 == 0)
+         {
+        	 retiringIn = 5;
+         }
+         else
+         {
+        	 retiringIn = 10;
+         }
+         
          
          
          
